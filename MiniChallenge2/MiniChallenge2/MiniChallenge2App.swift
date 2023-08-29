fa0342cbdbd35665991860e1b21ae6431e9421a8
lucascavalherie <lucascavalherie01@gmail.com>
2023-08-29 13:52:17 -0300

    creating project

---
+++ MiniChallenge2/MiniChallenge2/MiniChallenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge2App.swift
+//  MiniChallenge2
+//
+//  Created by Lucas Cavalherie on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
