73086aeda686221443b4b51c657406cd8c631998
lucascavalherie <lucascavalherie01@gmail.com>
2023-09-01 17:02:32 -0300

    Quiz view

--- MiniChallenge2/MiniChallenge2/Views/Quiz/QuizView.swift
+++ MiniChallenge2/MiniChallenge2/Views/Quiz/QuizView.swift
@@ -15,48 +15,86 @@ struct QuizView: View {
             if quizController.timeFinished{
                 ResultView()
             } else {
-                VStack{
-                    Spacer()
-                    
-                    Countdown(counter: $quizController.timer, countTo: quizController.totalTime){
-                        quizController.timeFinished = true
-                    }
-
-                    VStack(spacing: 16){
-                        VStack(spacing: 8){
-                            Text("Pergunta \(quizController.quiz.total):")
-                                .font(.subheadline)
-                                .foregroundColor(Color("DarkGray"))
-                                .fontWeight(.semibold)
-                            
-                            Text(quizController.question.text)
-                                .font(.headline)
-                                .multilineTextAlignment(.center)
-                                .frame(height: 100)
+                VStack {
+                    HStack (alignment: .center) {
+                        HStack {
+                            Image(systemName: "xmark")
+                                .font(.title)
                         }
+                        
+                        Spacer()
+                        
+                        HStack {
+                            Image(systemName: "sailboat.fill")
+                                .font(.title)
+                            
+                            Text("Vela")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .padding(.vertical, 2)
+                        }
+                        
+                        Spacer()
+                        
+                        HStack {
+                            Text(String(quizController.quiz.corrects))
+                        }
+                        .font(.headline)
+                        .foregroundColor(Color("White"))
+                        .padding(.horizontal)
+                        .padding(.vertical, 8)
+                        .background(Color("Green"))
+                        .cornerRadius(50)
                     }
-                    .foregroundColor(.black)
-                    .frame(maxWidth: .infinity)
-                    .padding(12)
+                    .padding(24)
                     .background(Color("White"))
-                    .cornerRadius(12)
-                    .padding(.bottom, 36)
+                    
+                    VStack{
+                        Spacer()
+                        
+                        Countdown(counter: $quizController.timer, countTo: quizController.totalTime){
+                            quizController.timeFinished = true
+                        }
 
-                    VStack(spacing: 16){
-                        ForEach(quizController.question.answers) { answer in
-                            AnswerButton(answer: .constant(answer)){
-                                quizController.answeringTheQuestion(answer: answer)
+                        VStack(spacing: 16){
+                            VStack(spacing: 8){
+                                Text("Pergunta \(quizController.quiz.total):")
+                                    .font(.subheadline)
+                                    .foregroundColor(Color("DarkGray"))
+                                    .fontWeight(.semibold)
+                                
+                                Text(quizController.question.text)
+                                    .font(.headline)
+                                    .multilineTextAlignment(.center)
+                                    .frame(height: 100)
                             }
                         }
-                    }
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity)
+                        .padding(12)
+                        .background(Color("White"))
+                        .cornerRadius(12)
+                        .padding(.bottom, 36)
 
-                    Spacer()
+                        VStack(spacing: 16){
+                            ForEach(quizController.question.answers) { answer in
+                                AnswerButton(answer: .constant(answer)){
+                                    quizController.answeringTheQuestion(answer: answer)
+                                }
+                            }
+                        }
+
+                        Spacer()
+                    }
+                    .padding(.horizontal, 24)
+                    .onAppear{quizController.generateFirstQuestion()}
+                    .background(Color("Blue"))
+                    .navigationBarBackButtonHidden()
                 }
-                .padding(.horizontal, 24)
-                .onAppear{quizController.generateFirstQuestion()}
-                .background(Color("Blue"))
-                .navigationBarBackButtonHidden()
-            }
+                }
+                
+                
+                
         }
     }
 }
