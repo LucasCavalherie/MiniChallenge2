a216de14181ead7e8e19245e6e576c3b008e8847
lucascavalherie <lucascavalherie01@gmail.com>
2023-08-31 22:09:49 -0300

    adding a default sports values

--- MiniChallenge2/MiniChallenge2/Datas/SportsData.swift
+++ MiniChallenge2/MiniChallenge2/Datas/SportsData.swift
@@ -8,6 +8,23 @@
 import Foundation
 
 final class SportsData{
-    var sport: [Sport] = []
+    var sport: [Sport] = [
+        Sport(
+            name: "Vela",
+            imageName: "Vela",
+            symbolName: "sailboat.fill",
+            link: "https://olympics.com/pt/esportes/vela/",
+            countryScoreOrder: [
+                Country(name: "Espanha", flagEmoji: "🇪🇸"),
+                Country(name: "Estados Unidos", flagEmoji: "🇺🇸"),
+                Country(name: "Japão", flagEmoji: "🇯🇵"),
+                Country(name: "Itália", flagEmoji: "🇮🇹"),
+                Country(name: "França", flagEmoji: "🇫🇷"),
+                Country(name: "Alemanha", flagEmoji: "🇩🇪"),
+                Country(name: "Argentina", flagEmoji: "🇦🇷")
+            ]
+        )
+    
+    ]
 }
 
