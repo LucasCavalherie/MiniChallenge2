b43927180b8350d6d976a5211464882135fad7e9
Ligia Naomi <ligianaominakase@gmail.com>
2023-09-06 13:54:30 -0300

    Alo

---
+++ MiniChallenge2/MiniChallenge2/Views/Onboarding/Onboarding1.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding1.swift
+//  MiniChallenge2
+//
+//  Created by Ligia Naomi Nakase on 06/09/23.
+//
+
+import SwiftUI
+
+struct Onboarding1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding1_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding1()
+    }
+}
