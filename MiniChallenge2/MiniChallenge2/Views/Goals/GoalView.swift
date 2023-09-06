150d2c5f3d905161f3302968b7c79d90e9b447bc
jadyyyy <jady@alunos.utfpr.edu.br>
2023-09-06 15:52:34 -0300

    criação da view

---
+++ MiniChallenge2/MiniChallenge2/Views/Goals/GoalView.swift
@@ -0,0 +1,20 @@
+//
+//  GoalView.swift
+//  MiniChallenge2
+//
+//  Created by Jady Torralvo on 06/09/23.
+//
+
+import SwiftUI
+
+struct GoalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GoalView_Previews: PreviewProvider {
+    static var previews: some View {
+        GoalView()
+    }
+}
