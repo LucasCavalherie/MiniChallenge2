77bbdb29f029db4e9536229cb061007170456c31
Ligia Naomi <ligianaominakase@gmail.com>
2023-09-09 22:52:48 -0300

    Onboarding Screens

---
+++ MiniChallenge2/MiniChallenge2/Views/Onboarding/Onboarding2.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding2.swift
+//  MiniChallenge2
+//
+//  Created by Ligia Naomi Nakase on 09/09/23.
+//
+
+import SwiftUI
+
+struct Onboarding2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding2_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding2()
+    }
+}
---
+++ MiniChallenge2/MiniChallenge2/Views/Onboarding/Onboarding4.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding4.swift
+//  MiniChallenge2
+//
+//  Created by Ligia Naomi Nakase on 09/09/23.
+//
+
+import SwiftUI
+
+struct Onboarding4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding4_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding4()
+    }
+}
