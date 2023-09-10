7152b9ac3c72d6069d7e1e891e9d10bca858250d
Ligia Naomi <ligianaominakase@gmail.com>
2023-09-10 12:31:13 -0300

    Onboarding e logo view

---
+++ MiniChallenge2/MiniChallenge2/Views/Logo/LogoView.swift
@@ -0,0 +1,20 @@
+//
+//  LogoView.swift
+//  MiniChallenge2
+//
+//  Created by Ligia Naomi Nakase on 10/09/23.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+    }
+}
